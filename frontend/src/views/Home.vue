23d0f0760ccd6091e1a0b6a56e606857fcdb6134
Jan Vargovský <jan.vargovsky@gmail.com>
2019-02-21 15:20:32 +0100

    Home page - timeline with basic flow.

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,13 +1,100 @@
 <template>
-  <HelloWorld/>
+  <v-layout row wrap>
+    <v-flex xs12>
+      <v-timeline align-top>
+        <v-timeline-item
+          v-for="(item, i) in items"
+          :key="i"
+          :color="item.color"
+          :icon="item.icon"
+          fill-dot
+        >
+          <v-card :color="item.color" dark>
+            <v-card-title class="title">{{ item.title }}</v-card-title>
+            <v-card-text class="white text--primary">
+              <p>{{ item.description }}</p>
+              <v-btn
+                v-for="(link, i) in item.links"
+                :key="i"
+                :color="item.color"
+                :to="link.to"
+                class="ml-0"
+                outline
+              >{{ link.name}}</v-btn>
+            </v-card-text>
+          </v-card>
+        </v-timeline-item>
+      </v-timeline>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
-
 export default {
-  components: {
-    HelloWorld
+  data() {
+    return {
+      items: [
+        {
+          title: "Data",
+          description: "We need a data to operate on. Upload a new dataset with your data.",
+          color: "indigo",
+          icon: "mdi-database-plus",
+          links: [
+            {
+              to: "/datasets/upload",
+              name: "Upload"
+            }
+          ]
+        },
+        {
+          title: "Preprocess",
+          description:
+            "We need a clean data, without any missing values or outlayers. Use preprocessing to remove those defects.",
+          color: "red",
+          icon: "mdi-settings-outline",
+          links: [
+            {
+              to: "/preprocessing/modify",
+              name: "Modify"
+            },
+            {
+              to: "/preprocessing/split",
+              name: "Split"
+            }
+          ]
+        },
+        {
+          title: "Review statistics",
+          description:
+            "Review exploration data analysis of a dataset. Apply your preprocess configurations.",
+          color: "green",
+          icon: "mdi-chart-bar",
+          links: [
+            {
+              to: "/statistics",
+              name: "Statistics"
+            }
+          ]
+        },
+        {
+          title: "Create model",
+          description:
+            "Create a model for your data. See how accurate it is against an unseen data.",
+          color: "teal",
+          icon: "mdi-cube-outline",
+          links: [
+            {
+              to: "models/create-simple",
+              name: "Create simple"
+            },
+            {
+              to: "/models/create",
+              name: "Create"
+            }
+          ]
+        }
+      ]
+    };
   }
 };
 </script>
