a41650a91eaa9ba5ad9aee4fba8f4178538feacd
Jan Vargovský <jan.vargovsky@gmail.com>
2019-02-21 15:27:39 +0100

    About page.

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,3 +1,11 @@
 <template>
-  <div class="about"><h1>This is an about page</h1></div>
+  <v-card>
+    <v-toolbar color="primary" dark flat dense card>
+      <v-toolbar-title class="subheading">About</v-toolbar-title>
+    </v-toolbar>
+    <v-card-text>
+      <p>Application was created as a master thesis at the VSB-TUO in 2019.</p>
+      <p>Author: Jan Vargovský</p>
+    </v-card-text>
+  </v-card>
 </template>
