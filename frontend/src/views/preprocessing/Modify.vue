18fdb7342e55d86afad2ce28cb2d6de91ae432a1
Jan Vargovský <jan.vargovsky@gmail.com>
2019-02-22 21:43:44 +0100

    Modify and split views - remove unnecesary elements.

--- frontend/src/views/preprocessing/Modify.vue
+++ frontend/src/views/preprocessing/Modify.vue
@@ -1,181 +1,168 @@
 <template>
-  <v-layout row wrap>
-    <v-flex xs12>
-      <v-card>
-        <v-toolbar color="red" dark flat dense card>
-          <v-toolbar-title
-            class="subheading"
-          >Modify dataset - remove, fix missing, normalize or encode values</v-toolbar-title>
-        </v-toolbar>
-        <v-card-text>
-          <v-form>
-            <v-layout row wrap>
-              <v-flex xs12>
-                <v-subheader class="pa-0">Dataset</v-subheader>
-                <v-overflow-btn
-                  v-model="dataset"
-                  :items="datasets"
-                  label="Select dataset to modify"
-                  editable
-                  class="pt-0 mt-0"
+  <v-card>
+    <v-toolbar color="red" dark flat dense card>
+      <v-toolbar-title
+        class="subheading"
+      >Modify dataset - remove, fix missing, normalize or encode values</v-toolbar-title>
+    </v-toolbar>
+    <v-card-text>
+      <v-form>
+        <v-layout row wrap>
+          <v-flex xs12>
+            <v-subheader class="pa-0">Dataset</v-subheader>
+            <v-overflow-btn
+              v-model="dataset"
+              :items="datasets"
+              label="Select dataset to modify"
+              editable
+              class="pt-0 mt-0"
+            />
+          </v-flex>
+          <v-flex xs12 v-if="dataset">
+            <v-badge v-if="canRemoveTotalCount > 0" color="red" overlap>
+              <span slot="badge">{{ removedCount }}/{{ canRemoveTotalCount }}</span>
+              <v-btn @click="removeAll" :flat="!removedAll" color="red" dark>Remove</v-btn>
+            </v-badge>
+
+            <v-badge v-if="canNormalizeTotalCount > 0" color="orange" overlap>
+              <span slot="badge">{{ normalizedCount }}/{{ canNormalizeTotalCount }}</span>
+              <v-btn @click="normalizeAll" :flat="!normalizedAll" color="orange" dark>Normalize</v-btn>
+            </v-badge>
+
+            <v-badge v-if="canFixMissingValueTotalCount > 0" color="green" overlap>
+              <span slot="badge">{{ fixedMissingValueCount }}/{{ canFixMissingValueTotalCount }}</span>
+              <v-btn
+                @click="missingValuesFixAll"
+                :flat="!missingValuesFixedAll"
+                color="green"
+                dark
+              >Fix missing values</v-btn>
+            </v-badge>
+
+            <v-badge v-if="canEncodeTotalCount > 0" color="blue" overlap>
+              <span slot="badge">{{ encodedCount }}/{{ canEncodeTotalCount }}</span>
+              <v-btn @click="encodeAll" :flat="!encodedAll" color="blue" dark>Encode</v-btn>
+            </v-badge>
+          </v-flex>
+          <v-flex xs12 row v-for="(column) in columns" :key="column.name">
+            <v-subheader class="pa-0">{{ column.name }} ({{ column.type }})</v-subheader>
+            <!-- Remove -->
+            <v-switch v-model="column.remove" label="Remove" color="red"/>
+            <!-- Normalize -->
+            <v-layout row v-if="canNormalize(column)">
+              <v-flex shrink>
+                <v-switch v-model="column.normalize" label="Normalize" color="orange"/>
+              </v-flex>
+              <v-flex shrink>
+                <v-text-field
+                  v-if="column.normalize"
+                  v-model="column.normalizeRange.min"
+                  class="pl-3"
+                  label="Min"
+                  type="number"
                 />
               </v-flex>
-              <v-flex xs12 v-if="dataset">
-                <v-badge v-if="canRemoveTotalCount > 0" color="red" overlap>
-                  <span slot="badge">{{ removedCount }}/{{ canRemoveTotalCount }}</span>
-                  <v-btn @click="removeAll" :flat="!removedAll" color="red" dark>Remove</v-btn>
-                </v-badge>
-
-                <v-badge v-if="canNormalizeTotalCount > 0" color="orange" overlap>
-                  <span slot="badge">{{ normalizedCount }}/{{ canNormalizeTotalCount }}</span>
-                  <v-btn @click="normalizeAll" :flat="!normalizedAll" color="orange" dark>Normalize</v-btn>
-                </v-badge>
-
-                <v-badge v-if="canFixMissingValueTotalCount > 0" color="green" overlap>
-                  <span slot="badge">{{ fixedMissingValueCount }}/{{ canFixMissingValueTotalCount }}</span>
-                  <v-btn
-                    @click="missingValuesFixAll"
-                    :flat="!missingValuesFixedAll"
-                    color="green"
-                    dark
-                  >Fix missing values</v-btn>
-                </v-badge>
-
-                <v-badge v-if="canEncodeTotalCount > 0" color="blue" overlap>
-                  <span slot="badge">{{ encodedCount }}/{{ canEncodeTotalCount }}</span>
-                  <v-btn @click="encodeAll" :flat="!encodedAll" color="blue" dark>Encode</v-btn>
-                </v-badge>
-              </v-flex>
-              <v-flex xs12 row v-for="(column) in columns" :key="column.name">
-                <v-subheader class="pa-0">{{ column.name }} ({{ column.type }})</v-subheader>
-                <!-- Remove -->
-                <v-switch v-model="column.remove" label="Remove" color="red"/>
-                <!-- Normalize -->
-                <v-layout row v-if="canNormalize(column)">
-                  <v-flex shrink>
-                    <v-switch v-model="column.normalize" label="Normalize" color="orange"/>
-                  </v-flex>
-                  <v-flex shrink>
-                    <v-text-field
-                      v-if="column.normalize"
-                      v-model="column.normalizeRange.min"
-                      class="pl-3"
-                      label="Min"
-                      type="number"
-                    />
-                  </v-flex>
-                  <v-flex shrink>
-                    <v-text-field
-                      v-if="column.normalize"
-                      v-model="column.normalizeRange.max"
-                      class="pl-3"
-                      label="Max"
-                      type="number"
-                    />
-                  </v-flex>
-                </v-layout>
-                <!-- Missing values -->
-                <v-layout row v-if="canFixNA(column)">
-                  <v-flex shrink>
-                    <v-switch v-model="column.na" label="Fix missing values" color="green"/>
-                  </v-flex>
-                  <v-flex shrink>
-                    <v-radio-group v-if="column.na" v-model="column.naMethod" row class="pl-3">
-                      <v-radio v-if="isNumber(column)" label="Using 0" value="zero" color="green"/>
-                      <v-radio
-                        v-if="isNumber(column)"
-                        label="Using average"
-                        value="average"
-                        color="green"
-                      />
-                      <v-radio label="Using the most common" value="common" color="green"/>
-                      <v-radio label="Custom" value="custom" color="green"/>
-                    </v-radio-group>
-                  </v-flex>
-                  <v-flex shrink>
-                    <v-text-field
-                      v-if="column.na && column.naMethod === 'custom'"
-                      v-model="column.naCustomValue"
-                      class="pl-3"
-                      label="Custom value"
-                      placeholder="Type here"
-                      :type="isNumber(column) ? 'number' : 'text'"
-                    />
-                  </v-flex>
-                </v-layout>
-                <!-- Categorical -->
-                <v-layout row v-if="canEncode(column)">
-                  <v-flex shrink>
-                    <v-switch v-model="column.encode" label="Encode" color="blue"/>
-                  </v-flex>
-                  <v-flex shrink>
-                    <v-radio-group
-                      v-if="column.encode"
-                      v-model="column.encodeMethod"
-                      row
-                      class="pl-3"
-                    >
-                      <v-radio label="Label/Integer encoding" value="label" color="blue"/>
-                      <v-radio label="One-Hot encoding" value="oneHot" color="blue"/>
-                    </v-radio-group>
-                  </v-flex>
-                </v-layout>
-              </v-flex>
-              <v-flex xs12 v-if="dataset">
-                <v-layout>
-                  <v-checkbox v-model="useNewDatasetName" hide-details class="shrink"/>
-                  <v-text-field
-                    v-model="newDatasetName"
-                    :disabled="!useNewDatasetName"
-                    label="New modified dataset name"
-                  />
-                </v-layout>
-                <v-layout>
-                  <v-checkbox v-model="useConfigurationName" hide-details class="shrink"/>
-                  <v-text-field
-                    v-model="configurationName"
-                    :disabled="!useConfigurationName"
-                    label="Configuration name"
-                  />
-                </v-layout>
-              </v-flex>
-              <v-flex xs12 v-if="dataset">
-                <v-alert
-                  :value="done && useNewDatasetName && this.newDatasetName.length !== 0"
-                  type="success"
-                  transition="scale-transition"
-                >
-                  Dataset
-                  <strong>{{ newDatasetName }}</strong>
-                  has been successfully created.
-                </v-alert>
-                <v-alert
-                  :value="done && useConfigurationName && this.configurationName.length !== 0"
-                  type="success"
-                  transition="scale-transition"
-                >
-                  Configuration
-                  <strong>{{ configurationName }}</strong>
-                  has been successfully created.
-                </v-alert>
-                <v-alert
-                  :value="submitError"
-                  type="error"
-                  transition="scale-transition"
-                >Server error.</v-alert>
-                <v-btn
-                  :disabled="disableSubmit"
-                  :loading="submitLoading"
-                  @click="submit"
-                  color="primary"
-                >Submit</v-btn>
+              <v-flex shrink>
+                <v-text-field
+                  v-if="column.normalize"
+                  v-model="column.normalizeRange.max"
+                  class="pl-3"
+                  label="Max"
+                  type="number"
+                />
               </v-flex>
             </v-layout>
-          </v-form>
-        </v-card-text>
-      </v-card>
-    </v-flex>
-  </v-layout>
+            <!-- Missing values -->
+            <v-layout row v-if="canFixNA(column)">
+              <v-flex shrink>
+                <v-switch v-model="column.na" label="Fix missing values" color="green"/>
+              </v-flex>
+              <v-flex shrink>
+                <v-radio-group v-if="column.na" v-model="column.naMethod" row class="pl-3">
+                  <v-radio v-if="isNumber(column)" label="Using 0" value="zero" color="green"/>
+                  <v-radio
+                    v-if="isNumber(column)"
+                    label="Using average"
+                    value="average"
+                    color="green"
+                  />
+                  <v-radio label="Using the most common" value="common" color="green"/>
+                  <v-radio label="Custom" value="custom" color="green"/>
+                </v-radio-group>
+              </v-flex>
+              <v-flex shrink>
+                <v-text-field
+                  v-if="column.na && column.naMethod === 'custom'"
+                  v-model="column.naCustomValue"
+                  class="pl-3"
+                  label="Custom value"
+                  placeholder="Type here"
+                  :type="isNumber(column) ? 'number' : 'text'"
+                />
+              </v-flex>
+            </v-layout>
+            <!-- Categorical -->
+            <v-layout row v-if="canEncode(column)">
+              <v-flex shrink>
+                <v-switch v-model="column.encode" label="Encode" color="blue"/>
+              </v-flex>
+              <v-flex shrink>
+                <v-radio-group v-if="column.encode" v-model="column.encodeMethod" row class="pl-3">
+                  <v-radio label="Label/Integer encoding" value="label" color="blue"/>
+                  <v-radio label="One-Hot encoding" value="oneHot" color="blue"/>
+                </v-radio-group>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+          <v-flex xs12 v-if="dataset">
+            <v-layout>
+              <v-checkbox v-model="useNewDatasetName" hide-details class="shrink"/>
+              <v-text-field
+                v-model="newDatasetName"
+                :disabled="!useNewDatasetName"
+                label="New modified dataset name"
+              />
+            </v-layout>
+            <v-layout>
+              <v-checkbox v-model="useConfigurationName" hide-details class="shrink"/>
+              <v-text-field
+                v-model="configurationName"
+                :disabled="!useConfigurationName"
+                label="Configuration name"
+              />
+            </v-layout>
+          </v-flex>
+          <v-flex xs12 v-if="dataset">
+            <v-alert
+              :value="done && useNewDatasetName && this.newDatasetName.length !== 0"
+              type="success"
+              transition="scale-transition"
+            >
+              Dataset
+              <strong>{{ newDatasetName }}</strong>
+              has been successfully created.
+            </v-alert>
+            <v-alert
+              :value="done && useConfigurationName && this.configurationName.length !== 0"
+              type="success"
+              transition="scale-transition"
+            >
+              Configuration
+              <strong>{{ configurationName }}</strong>
+              has been successfully created.
+            </v-alert>
+            <v-alert :value="submitError" type="error" transition="scale-transition">Server error.</v-alert>
+            <v-btn
+              :disabled="disableSubmit"
+              :loading="submitLoading"
+              @click="submit"
+              color="primary"
+            >Submit</v-btn>
+          </v-flex>
+        </v-layout>
+      </v-form>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
--- frontend/src/views/preprocessing/Split.vue
+++ frontend/src/views/preprocessing/Split.vue
@@ -1,63 +1,55 @@
 <template>
-  <v-layout row wrap>
-    <v-flex xs12>
-      <v-card>
-        <v-toolbar color="amber darken-3" dark flat dense>
-          <v-toolbar-title class="subheading">Split dataset into train and test</v-toolbar-title>
-        </v-toolbar>
-        <v-card-text>
-          <v-form>
-            <v-layout row wrap>
-              <v-flex xs12>
-                <v-subheader class="pa-0">Dataset</v-subheader>
-                <v-overflow-btn
-                  v-model="dataset"
-                  :items="datasets"
-                  label="Select dataset to split"
-                  editable
-                  class="pt-0 mt-0"
-                />
-              </v-flex>
-              <v-flex xs12 v-if="dataset">
-                <v-subheader class="pa-0">{{ rows }} rows to split</v-subheader>
-                <v-slider v-model="ratio" :min="min" :max="max" always-dirty></v-slider>
-              </v-flex>
-              <v-flex xs12 v-if="dataset">
-                <v-subheader
-                  class="pa-0"
-                >Split ratio ({{ trainPercent | formatPercent }} vs {{ testPercent | formatPercent }})</v-subheader>
-                <v-text-field v-model="train" readonly label="Train rows" :error="train == 0"></v-text-field>
-                <v-text-field v-model="test" readonly label="Test rows" :error="test == 0"></v-text-field>
-              </v-flex>
-              <v-flex xs12 v-if="dataset">
-                <v-subheader class="pa-0">Datasets names</v-subheader>
-                <v-text-field v-model="trainDatasetName" label="Train dataset name"></v-text-field>
-                <v-text-field v-model="testDatasetName" label="Test dataset name"></v-text-field>
-              </v-flex>
-              <v-flex xs12 v-if="dataset">
-                <v-alert :value="done" type="success" transition="scale-transition">
-                  Datasets
-                  <strong>{{ trainDatasetName }}</strong> and
-                  <strong>{{ testDatasetName }}</strong> has been successfully created.
-                </v-alert>
-                <v-alert
-                  :value="submitError"
-                  type="error"
-                  transition="scale-transition"
-                >Server error.</v-alert>
-                <v-btn
-                  color="primary"
-                  :disabled="train === 0 || test === 0"
-                  :loading="submitLoading"
-                  @click="submit"
-                >Split</v-btn>
-              </v-flex>
-            </v-layout>
-          </v-form>
-        </v-card-text>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-card>
+    <v-toolbar color="amber darken-3" dark flat dense>
+      <v-toolbar-title class="subheading">Split dataset into train and test</v-toolbar-title>
+    </v-toolbar>
+    <v-card-text>
+      <v-form>
+        <v-layout row wrap>
+          <v-flex xs12>
+            <v-subheader class="pa-0">Dataset</v-subheader>
+            <v-overflow-btn
+              v-model="dataset"
+              :items="datasets"
+              label="Select dataset to split"
+              editable
+              class="pt-0 mt-0"
+            />
+          </v-flex>
+          <v-flex xs12 v-if="dataset">
+            <v-subheader class="pa-0">{{ rows }} rows to split</v-subheader>
+            <v-slider v-model="ratio" :min="min" :max="max" always-dirty></v-slider>
+          </v-flex>
+          <v-flex xs12 v-if="dataset">
+            <v-subheader
+              class="pa-0"
+            >Split ratio ({{ trainPercent | formatPercent }} vs {{ testPercent | formatPercent }})</v-subheader>
+            <v-text-field v-model="train" readonly label="Train rows" :error="train == 0"></v-text-field>
+            <v-text-field v-model="test" readonly label="Test rows" :error="test == 0"></v-text-field>
+          </v-flex>
+          <v-flex xs12 v-if="dataset">
+            <v-subheader class="pa-0">Datasets names</v-subheader>
+            <v-text-field v-model="trainDatasetName" label="Train dataset name"></v-text-field>
+            <v-text-field v-model="testDatasetName" label="Test dataset name"></v-text-field>
+          </v-flex>
+          <v-flex xs12 v-if="dataset">
+            <v-alert :value="done" type="success" transition="scale-transition">
+              Datasets
+              <strong>{{ trainDatasetName }}</strong> and
+              <strong>{{ testDatasetName }}</strong> has been successfully created.
+            </v-alert>
+            <v-alert :value="submitError" type="error" transition="scale-transition">Server error.</v-alert>
+            <v-btn
+              color="primary"
+              :disabled="train === 0 || test === 0"
+              :loading="submitLoading"
+              @click="submit"
+            >Split</v-btn>
+          </v-flex>
+        </v-layout>
+      </v-form>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
@@ -92,12 +84,15 @@ export default {
         this.done = false;
         this.submitLoading = true;
         this.submitError = false;
-        await this.$http.put(`/api/v1/preprocessing/dataset/${this.dataset}/split`, {
-          ratio: this.ratio / Units,
-          shuffle: true,
-          trainDataset: this.trainDatasetName,
-          testDataset: this.testDatasetName
-        });
+        await this.$http.put(
+          `/api/v1/preprocessing/dataset/${this.dataset}/split`,
+          {
+            ratio: this.ratio / Units,
+            shuffle: true,
+            trainDataset: this.trainDatasetName,
+            testDataset: this.testDatasetName
+          }
+        );
         this.done = true;
       } catch {
         this.submitError = true;
